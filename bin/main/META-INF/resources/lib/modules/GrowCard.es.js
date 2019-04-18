4783da4b67efbdc474acc12e634407f5bd77d4fa
alffox <alfonso.crisci.it@gmail.com>
2019-04-18 08:36:01 +0200

    feat: Implement card's top row

--- bin/main/META-INF/resources/lib/modules/GrowCard.es.js
+++ bin/main/META-INF/resources/lib/modules/GrowCard.es.js
@@ -2,49 +2,52 @@ import React from 'react';
 
 const GrowCard = (props) => {
     return(
-        <div className="col-lg-4">
-            <div className="card-type-asset form-check form-check-card form-check-top-left image-card">
-                <div className="card">
-                    <div className="aspect-ratio card-item-first">
-                        <div className="custom-control custom-checkbox">
-                            <label>
-                                <input className="custom-control-input" type="checkbox"/>
-                                <span className="custom-control-label"></span>
-                                <img alt="thumbnail"className="aspect-ratio-item-center-middle aspect-ratio-item-fluid" src="https://via.placeholder.com/300x200" />
-                                <span className="sticker sticker-bottom-left sticker-danger rounded-circle">JPG</span>
-                            </label>
-                        </div>
-                    </div>
-                    <div className="card-body">
-                        <div className="card-row">
+        <div className="col-lg-12">
+            <div className="card">
+                <div className="card-body">
+
+                <div className="autofit-row autofit-padded">
+					<div className="autofit-col">
+						<div className="autofit-section">
+						<img alt="Author's thumbnail" className="img-fluid sticker sticker-primary sticker-xl rounded-circle" src={props.authorAvatar} />
+						</div>
+					</div>
+				<div className="autofit-col autofit-col-expand">
+					<div className="autofit-section">
+						<span>Author</span>
+						<br />
+						<span>Date</span>
+					</div>
+				</div>
+				<div className="autofit-col">
+					<div className="autofit-section">
+						<h6>Like - Save</h6>
+						<svg className="lexicon-icon lexicon-icon-star-o"></svg>
+					</div>
+				</div>
+		</div>
+
+                        {/* <div className="autofit-row autofit-padded">
+
                             <div className="autofit-col autofit-col-expand">
-                                <div className="card-title text-truncate" title="thumbnail_coffee.jpg">thumbnail_coffee.jpg</div>
-                                <div className="card-subtitle text-truncate" title="Author Action">Author Action</div>
-                                <div className="card-detail">
-                                    <span className="label label-success">
-                                        <span className="label-item label-item-expand">Approved</span>
-                                    </span>
+                                <div className="autofit-section">
+                                <img alt="Author's thumbnail" className="img-fluid sticker sticker-primary rounded-circle" src={props.authorAvatar} />
                                 </div>
-                            </div>
-                            <div className="autofit-col">
-                                <div className="dropdown dropdown-action">
-                                    <a aria-expanded="false" aria-haspopup="true" className="component-action dropdown-toggle" data-toggle="dropdown" href="#1" role="button">
-                                        <svg className="lexicon-icon lexicon-icon-ellipsis-v" focusable="false" role="presentation">
-                                            <use xlinkHref={props.spritemap+"/clay/icons.svg#ellipsis-v"} />
-                                        </svg>
-                                    </a>
-                                    <div className="dropdown-menu dropdown-menu-right">
-                                        <a className="dropdown-item" href="#1">Download</a>
-                                        <a className="dropdown-item" href="#1">Edit</a>
-                                        <a className="dropdown-item" href="#1">Move</a>
-                                        <a className="dropdown-item" href="#1">Checkout</a>
-                                        <a className="dropdown-item" href="#1">Permissions</a>
-                                        <a className="dropdown-item" href="#1">Move to Recycle Bin</a>
+
+                                <div className="autofit-col autofit-col-expand">
+                                <div className="autofit-section">
+                                    <h6>Author-date</h6>
+                                    </div>
+                                </div>
+
+                                <div className="autofit-col autofit-col-expand">
+                                    <div className="autofit-section">
+                                        <h6>star - like</h6>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
+                        </div> */}
+
                 </div>
             </div>
         </div>
